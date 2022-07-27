9f39ab2ef91987b8cb14327f755c629c93d38a06
yidafu <tiaozuwushi@qq.com>
2022-07-27 17:40:58 +0800

    feat: stylelint added

--- packages/water-plugin-lint/src/index.ts
+++ packages/water-plugin-lint/src/index.ts
@@ -1,7 +1,13 @@
+import path from 'path';
+import url from 'url';
 import { Plugin } from '@pure/api';
 import { ESLint } from 'eslint';
 import { runEslint } from './eslint';
+import { runStylelint } from './stylelint';
 import { ESLINT_CONFIG_MAP } from './eslint-config';
+import { STYLELINT_CONFIG } from './stylelint-config';
+import stylelint from 'stylelint';
+import { tryResolve } from '@pure/api/lib/utils';
 
 interface ILintPluginActionOption {
   fix: boolean;
@@ -10,10 +16,11 @@ interface ILintPluginActionOption {
 interface ILintPluginOption {
   type?: 'base' | 'vue' | 'vue3';
   eslintOption?: ESLint.Options,
+  stylelitOption?: stylelint.LinterOptions,
 }
 
 class LintPlugin extends Plugin {
-  registerCommand =  () => {
+  registerCommand = () => {
     const lintPlgOption: ILintPluginOption = this.getPluginOption('lint');
     return {
       name: 'lint',
@@ -21,12 +28,20 @@ class LintPlugin extends Plugin {
         '--fix': 'Automatically fix problems',
       },
       async action(option: ILintPluginActionOption) {
-        const { type, eslintOption = {} } = lintPlgOption;
+        const { type, eslintOption = {}, stylelitOption = {} } = lintPlgOption;
         const eslintConfig = ESLINT_CONFIG_MAP.get(type ?? 'base');
 
         await runEslint({
           ...eslintOption,
           baseConfig: eslintConfig,
+          fix: option.fix,
+        });
+
+        await runStylelint({
+          config: STYLELINT_CONFIG,
+          formatter: 'string',
+          files: ['src/**/*.css', 'src/**.scss'],
+          ...stylelitOption,
           fix: option.fix,
         });
       },
---
+++ packages/water-plugin-lint/src/stylelint.ts
@@ -0,0 +1,18 @@
+// 
+// "stylelint-config-standard-scss",
+// "stylelint-config-prettier-scss"
+import debug from 'debug';
+import stylelint from 'stylelint';
+
+const log = debug('pure:lint:stylelint');
+
+export async function runStylelint(options: stylelint.LinterOptions) {
+  try {
+    log('[start] stylelint %s', JSON.stringify(options));
+    const data = await stylelint.lint(options);
+    console.log(data.output);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
