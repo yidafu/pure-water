0c5991c98a71fca339d061de9be69816e3c83cbc
yidafu <tiaozuwushi@qq.com>
2022-08-02 10:32:45 +0800

    feat: project root config lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,3 @@
-// eslint-disable
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'airbnb-typescript/base'
-  ],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    extraFileExtensions: ['.vue'],
-    project: './tsconfig.json'
-  },
-  plugins: [
-    'import',
-    '@typescript-eslint',
-  ],
-  rules: {},
-};
+  extends: ['@pure-org/eslint-config-water/vue']
+}
