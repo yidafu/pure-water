4f57bd877de9925ca29f00f017ce66351c5e3e5b
yidafu <tiaozuwushi@qq.com>
2022-08-02 10:31:37 +0800

    feat(plugin): lint spinner power by ora

--- packages/water-plugin-lint/src/eslint.ts
+++ packages/water-plugin-lint/src/eslint.ts
@@ -1,20 +1,26 @@
 import { ESLint } from 'eslint';
+import ora from 'ora';
 import debug from 'debug';
+
 const log = debug('pure:plugin:lint:eslint');
 
-export async function runEslint(option: ESLint.Options) {
+export async function runEslint(entry: string[], option: ESLint.Options) {
+  const spinner = ora({
+    text: '开始 ESlint ...',
+    spinner: 'dots8Bit',
+    color: 'green',
+  }).start();
   try {
     log('[start] eslint %s', JSON.stringify(option));
     const eslint = new ESLint(option);
 
-    // const config = await eslint.calculateConfigForFile('/mnt/d/gitlab/pure-water/packages/water-plugin-lint/test.js');
+    // const config = await eslint.calculateConfigForFile(
+    //   '/mnt/d/gitlab/pure-water/packages/water-plugin-lint/test.js'
+    // );
     // const { rules, ...cfg } = config;
     // console.log(cfg);
-
-    const results = await eslint.lintFiles([
-      'src/**/*.ts',
-      'src/**/*.vue',
-    ]);
+    log('eslint entry => %s', entry);
+    const results = await eslint.lintFiles(entry);
 
     if (option.fix) {
       log('[running] eslint autofix');
@@ -23,16 +29,25 @@ export async function runEslint(option: ESLint.Options) {
 
     const formatter = await eslint.loadFormatter('stylish');
 
-    const resultText = await formatter.format(results);
-    console.log(resultText);
-    log('[end] eslint');
-
     // 如果 lint 报错直接退出
-    const hasError = results.find(res => res.errorCount > 0 || res.fatalErrorCount > 0);
+    const hasError = results.find((res) => res.errorCount > 0 || res.fatalErrorCount > 0);
+    if (hasError) {
+      spinner.fail('ESlint 校验失败');
+    } else {
+      spinner.succeed('ESLint 校验通过');
+    }
+
+    const resultText = await formatter.format(results);
+    if (resultText) {
+      console.log(resultText);
+    }
+
+    log('[end] eslint');
     if (hasError) {
       process.exit(1);
     }
   } catch (err) {
+    spinner.fail('ESlint 校验失败');
     console.error(err);
     process.exit(1);
   }
--- packages/water-plugin-lint/src/stylelint.ts
+++ packages/water-plugin-lint/src/stylelint.ts
@@ -1,17 +1,27 @@
-// 
-// "stylelint-config-standard-scss",
-// "stylelint-config-prettier-scss"
+import ora from 'ora';
 import debug from 'debug';
 import stylelint from 'stylelint';
 
 const log = debug('pure:lint:stylelint');
 
 export async function runStylelint(options: stylelint.LinterOptions) {
+  const spinner = ora({
+    text: '开始 Style Lint ...',
+    spinner: 'dots8Bit',
+    color: 'green',
+  }).start();
+
   try {
     log('[start] stylelint %s', JSON.stringify(options));
     const data = await stylelint.lint(options);
-    
-    console.log(data.output);
+    if (data.errored) {
+      spinner.fail('Style Lint 校验失败');
+    } else {
+      spinner.succeed('Style Lint 校验通过');
+    }
+    if (data.output) {
+      console.log(data.output);
+    }
     log('[end] styleint');
     if (data.errored) {
       process.exit(1);
