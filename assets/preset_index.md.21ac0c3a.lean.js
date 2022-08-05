96cba7b3d51ace06cffc9d686787de7bea0104c2
yidafu <yidafu@users.noreply.github.com>
2022-08-05 14:29:13 +0000

    deploy: 5020cefd73e2744342f98fdf29adfb3a4ad07aac

--- assets/preset_index.md.21ac0c3a.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o,a as r}from"./app.eb734d83.js";const u=JSON.parse('{"title":"Preset","description":"","frontmatter":{},"headers":[],"relativePath":"preset/index.md"}'),c={name:"preset/index.md"},d=r("",4),s=[d];function a(n,p,_,i,f,h){return o(),t("div",null,s)}var m=e(c,[["render",a]]);export{u as __pageData,m as default};
